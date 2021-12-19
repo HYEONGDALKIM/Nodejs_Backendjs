6e95cfb6fcd9afb882c848a32d196b67ae308853
HYEONGDALKIM <guddak@kakao.com>
2021-12-19 19:21:04 +0900

    .

--- router/join/index.js
+++ router/join/index.js
@@ -3,7 +3,11 @@ var app = express()
 var router = express.Router()
 var path = require('path') // 상대경로
 var mysql = require('mysql') // mysql 모듈 가져오기
-const passport = require('passport')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash')
+
 
 //DATABASE SETTING
 var connection = mysql.createConnection({
@@ -19,21 +23,19 @@ router.get('/', function(req,res){
   console.log('get join url');
   res.render('join.ejs')
 })
+
 //post >> passport use
 // router.post('/', function(req,res){
 //     var body = req.body;
 //     var email = body.email;
 //     var name = body.name;
 //     var pwd = body.password;
-
 //     var sql = {email: email, name : name, pw: pwd};
 //     var query = connection.query('insert into user set ?', sql, function(err,rows){
 //         if(err) throw err;
 //         else res.render('welcome.ejs', {'name': name, 'id':rows.insertId})
 //     })
 // })
-
-
 
 passport.use('local-join', new LocalStrategy({
     usernameField : 'email',
@@ -43,6 +45,5 @@ passport.use('local-join', new LocalStrategy({
     console.log('local-join callback called');
 }
 ));
-
 
 module.exports = router;
