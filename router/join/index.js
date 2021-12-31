b4be7f2398efa92ac476df495269739097699b06
HYEONGDALKIM <guddak@kakao.com>
2021-12-31 15:48:53 +0900

    .

--- router/join/index.js
+++ router/join/index.js
@@ -1,32 +1,30 @@
-var express = require('express') 
-var app = express()
-var router = express.Router()
-var path = require('path') // 상대경로
-var mysql = require('mysql') // mysql 모듈 가져오기
-var passport = require('passport')
-var LocalStrategy = require('passport-local').Strategy
-var session = require('express-session')
-var flash = require('connect-flash')
-
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = require("path"); // 상대경로
+var mysql = require("mysql"); // mysql 모듈 가져오기
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+var session = require("express-session");
+var flash = require("connect-flash");
 
 //DATABASE SETTING
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    port     :  3306,
-    user     : 'root',
-    password : 'wodo1965~!',
-    database : 'jsman'
-  })
-  connection.connect();
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "wodo1965~!",
+  database: "jsman",
+});
+connection.connect();
 
-router.get('/', function(req,res){ 
+router.get("/", function (req, res) {
   var msg;
-  var errMsg = req.flash('error')
-  
-  if(errMsg) msg = errMsg;
-  res.render('join.ejs', {'message': msg});
-})
+  var errMsg = req.flash("error");
 
+  if (errMsg) msg = errMsg;
+  res.render("join.ejs", { message: msg });
+});
 
 //post >> passport use
 // router.post('/', function(req,res){
@@ -41,29 +39,39 @@ router.get('/', function(req,res){
 //     })
 // })
 
-passport.use('local-join', new LocalStrategy({
-    usernameField : 'email',
-    passwordField: 'password',
-    passReqToCallback : true
-}, function(req, email, password, done){
-    var query = connection.query('select *from user where email=?', [email], function(err,rows){
-      if(err) return done(err);
+passport.use(
+  "local-join",
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true,
+    },
+    function (req, email, password, done) {
+      var query = connection.query(
+        "select *from user where email=?",
+        [email],
+        function (err, rows) {
+          if (err) return done(err);
 
-      if(rows.length){
-        console.log('Oh!!! existed user')
-        return done(null, false, {message: 'your email is already used'})
-      }else{
+          if (rows.length) {
+            console.log("Oh!!! existed user");
+            return done(null, false, { message: "your email is already used" });
+          } else {
+          }
+        }
+      );
+    }
+  )
+);
 
-      
-      }
-    })
-  }
-));
-
-router.post('/', passport.authenticate('local-join', {
-      successRedirect: '/main',
-      failureRedirect: '/join',
-      failureFlash:true })
-)
+router.post(
+  "/",
+  passport.authenticate("local-join", {
+    successRedirect: "/main",
+    failureRedirect: "/join",
+    failureFlash: true,
+  })
+);
 
 module.exports = router;
